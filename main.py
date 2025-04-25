7660b3fb7320699a5a5c602fe6c960f21b4869f5
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-25 21:41:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from handlers import start, profile, percent, study, coins, ask_question
 from admin import panel
 from dotenv import load_dotenv
@@ -11,7 +12,10 @@ from db import init_db
 load_dotenv()
 
 # Initialize bot and dispatcher
-bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=os.getenv("BOT_TOKEN"),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 # Register routers
