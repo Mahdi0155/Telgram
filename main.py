c37b0fc80f996381a5b058cb1e15ea591fcf81c8
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-25 18:12:17 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from handlers import start, profile, percent, study, help, coins
+from admin import panel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+
+# Register routers
+dp.include_routers(
+    start.router,
+    profile.router,
+    percent.router,
+    study.router,
+    help.router,
+    coins.router,
+    panel.router,
+)
+
+async def main():
+    print("ربات آماده است...")
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
